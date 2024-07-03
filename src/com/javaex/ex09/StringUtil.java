d7e17aa9ec174f622dd346faae040736811a5e12
JeffJPortfolio <jeffjeon.official@gmail.com>
2024-07-03 17:43:49 +0900

    ex09

--- src/com/javaex/ex09/StringUtil.java
+++ src/com/javaex/ex09/StringUtil.java
@@ -2,10 +2,14 @@ package com.javaex.ex09;
 
 public class StringUtil {
     
-    public static String concatString(){
+    public static String concatString(String[] arr){
        
         //메소드 내용작성
-        
+        String ans = " ";
+        for (int i=0; i < arr.length ; i++) {
+        	ans = ans + arr[i];
+        }
+        return  ans;
     }
 
 }
