359ca7f6f03f7c9cf05c70346a78a75a49b965c4
JeffJPortfolio <jeffjeon.official@gmail.com>
2024-07-03 16:30:06 +0900

    Ex03

--- src/com/javaex/ex03/SongApp.java
+++ src/com/javaex/ex03/SongApp.java
@@ -3,6 +3,10 @@ package com.javaex.ex03;
 public class SongApp {
 
 	public static void main(String[] args){
+		//파라미터 5
+		//파라미터 6
+		
+		Song s1 = new Song("좋은날", "아이유", "Real", "이민수", "2010", 3);
 		
 		
 	}
